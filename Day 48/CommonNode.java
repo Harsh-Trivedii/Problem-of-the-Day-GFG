1e90b041b9e110cc6d1f3562d0224ec7cb37f5d4
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2023-10-14 17:55:39 +0530

    Create CommonNode.java

---
+++ Day 48/CommonNode.java
@@ -0,0 +1,156 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node{
+    int data;
+    Node left, right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution {
+    // Function to find the nodes that are common in both BSTs.
+    public static ArrayList<Integer> findCommon(Node root1, Node root2) {
+        ArrayList<Integer> result = new ArrayList<>();
+        inOrderTraversal(root1, root2, result);
+        return result;
+    }
+
+    private static void inOrderTraversal(Node root1, Node root2, ArrayList<Integer> result) {
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+
+        while (!stack1.isEmpty() || !stack2.isEmpty() || root1 != null || root2 != null) {
+            while (root1 != null) {
+                stack1.push(root1);
+                root1 = root1.left;
+            }
+            while (root2 != null) {
+                stack2.push(root2);
+                root2 = root2.left;
+            }
+
+            if (!stack1.isEmpty() && !stack2.isEmpty()) {
+                Node top1 = stack1.peek();
+                Node top2 = stack2.peek();
+
+                if (top1.data == top2.data) {
+                    result.add(top1.data);
+                    stack1.pop();
+                    stack2.pop();
+                    root1 = top1.right;
+                    root2 = top2.right;
+                } else if (top1.data < top2.data) {
+                    stack1.pop();
+                    root1 = top1.right;
+                } else {
+                    stack2.pop();
+                    root2 = top2.right;
+                }
+            } else if (!stack1.isEmpty()) {
+                root1 = stack1.pop();
+                root1 = root1.right;
+            } else if (!stack2.isEmpty()) {
+                root2 = stack2.pop();
+                root2 = root2.right;
+            }
+        }
+    }
+}
+
+//{ Driver Code Starts.
+class CommonNode
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+            
+            s = br.readLine();
+            Node root2 = buildTree(s);
+            
+            Solution g = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = g.findCommon(root1,root2);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+            
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
