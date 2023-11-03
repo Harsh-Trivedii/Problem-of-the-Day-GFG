d75e06a6591c3b17f2d34fbba3ecf69a69e8e58b
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2023-11-03 13:05:12 +0530

    Create Pythagorean.java

---
+++ Day 68/Pythagorean.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Pythagorean {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().checkTriplet(arr, n);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+        int maxNum = Integer.MIN_VALUE;
+        for (int num : arr) {
+            maxNum = Math.max(maxNum, num);
+        }
+        
+        int maxNumSquare = maxNum * maxNum;
+        HashSet<Integer> squareSet = new HashSet<>();
+
+        for (int num : arr) {
+            squareSet.add(num * num);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int sumOfSquares = arr[i] * arr[i] + arr[j] * arr[j];
+                if (sumOfSquares <= maxNumSquare && squareSet.contains(sumOfSquares)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
