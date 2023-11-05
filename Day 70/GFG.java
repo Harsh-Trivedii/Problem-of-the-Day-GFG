5019ff6fcdb3927044fea53ae60c789a015cd8d3
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2023-11-05 12:48:52 +0530

    Create GFG.java

---
+++ Day 70/GFG.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = Integer.parseInt(s[i + 1]);
+            }
+            int k = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            int[] ans = obj.topK(nums, k);
+            for (int i = 0; i < ans.length; i++) System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int[] topK(int[] nums, int k) {
+        // Code here
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Create a list of entries from the frequencyMap
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(frequencyMap.entrySet());
+
+        // Sort the entryList based on frequency and value
+        Collections.sort(entryList, (a, b) -> {
+            if (a.getValue() == b.getValue()) {
+                return b.getKey() - a.getKey(); // Sort by value in descending order
+            } else {
+                return b.getValue() - a.getValue(); // Sort by frequency in descending order
+            }
+        });
+
+        // Create the result array with the top k elements
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = entryList.get(i).getKey();
+        }
+
+        return result;
+    }
+}
