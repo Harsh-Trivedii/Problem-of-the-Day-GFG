ee56906835d3cde6c76c6a6ebc553a70c0053b0e
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2023-11-22 12:12:17 +0530

    Create SymmetricTree.java

---
+++ Day 87/SymmetricTree.java
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class SymmetricTree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                GfG g = new GfG();
+                if(g.isSymmetric(root) == true)
+                    System.out.println("True");
+                else
+                    System.out.println("False");
+        	
+                t--;
+            
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+/*
+class of the node of the tree is as
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+*/
+// complete this function
+// return true/false if the is Symmetric or not
+class GfG
+{
+    // return true/false denoting whether the tree is Symmetric or not
+   private static boolean isMirror(Node left, Node right) {
+        // If both trees are empty, they are symmetric
+        if (left == null && right == null)
+            return true;
+
+        // If one of the trees is empty and the other is not, they are not symmetric
+        if (left == null || right == null)
+            return false;
+
+        // Check if the values of the current nodes are equal and if the left subtree of the left
+        // node is symmetric to the right subtree of the right node, and vice versa
+        return (left.data == right.data) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+
+    // Main function to check if the tree is symmetric
+    public static boolean isSymmetric(Node root)
+    {
+        // If the tree is empty, it is symmetric
+        if (root == null)
+            return true;
+
+        // Check if the left and right subtrees are symmetric
+        return isMirror(root.left, root.right);
+    }
+}
