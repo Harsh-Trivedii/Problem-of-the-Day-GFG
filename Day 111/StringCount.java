0877918c12df48363bff59313a6f67b107fc6207
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2023-12-16 21:43:28 +0530

    Create StringCount.java

---
+++ Day 111/StringCount.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+
+class StringCount{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+  
+        
+        int t = Integer.parseInt(br.readLine().trim()); 
+
+        while(t > 0){
+        	long n = Integer.parseInt(br.readLine().trim()); 
+            Solution ob = new Solution();
+            System.out.println(ob.countStr(n)); 
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    static long countStr(long n) {
+	  if(n== 1){
+        return 3;
+    }
+    if(n== 2){
+        return 8;
+    }
+    long ans= (n*n*n+ 3*n+ 2)/2;
+    return ans;
+	}
+} 
