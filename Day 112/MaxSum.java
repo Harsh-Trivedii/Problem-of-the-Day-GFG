3c395a74cc2a0cc10bef6b0a4a2fe7fd70b63f59
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2023-12-17 20:35:48 +0530

    Create MaxSum.java

---
+++ Day 112/MaxSum.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class MaxSum {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaxSum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int findMaxSum(int arr[], int n) {
+        if (n == 1) {
+            return arr[0];
+        }
+        
+        int incl = arr[0];
+        int excl = 0;
+        int excl_new;
+        
+        for (int i = 1; i < n; i++) {
+            excl_new = Math.max(incl, excl);
+            
+            incl = excl + arr[i];
+            excl = excl_new;
+        }
+        
+        return Math.max(incl, excl);
+    }
+}
