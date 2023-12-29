58b2a2240bcd96fe803c41e42e1a0faa3a23c2a7
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2023-12-29 11:56:05 +0530

    Create StringRepetition.java

---
+++ Day 124/StringRepetition.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class StringRepetition
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            String s = sc.next();
+            int k = sc.nextInt();
+            int answer = new Solution().kSubstrConcat(n,s,k);
+    		System.out.println(answer);
+        }
+        
+    }
+}
+
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution
+{
+    int kSubstrConcat(int n, String s, int k)
+    {
+       if(n%k!=0) return 0;
+        HashSet<String> set=new HashSet<>();
+        for(int i=0;i<n;i+=k){
+            set.add(s.substring(i,i+k));
+        }
+        return set.size()<=2?1:0;
+    }
+}
